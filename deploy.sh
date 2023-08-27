2aa2ab9762451a7e61624962dd974f1a9a12a593
insanium-tt <insanium-foilings@protonmail.com>
2023-08-27 01:49:52 -0400

    site update: 08/27/23 01:49:52

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+dir="/home/synchronous/code/insanium/static/"
+cd dir
+
 dt=$(date +"%D %T")
 lol="site update: ${dt}"
 
-git --git-dir /home/synchronous/code/insanium/static/.git add .
+git --git-dir /home/synchronous/code/insanium/static/.git add /home/synchronous/insanium/static/*
 git --git-dir /home/synchronous/code/insanium/static/.git commit -am "$lol" 
 git --git-dir /home/synchronous/code/insanium/static/.git push origin master
